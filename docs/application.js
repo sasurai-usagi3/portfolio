7a453cbc6df3841c47ab93cad4e55bc20d8f355b
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-05-22 08:03:38 +0900

    add: page内リンク

--- docs/application.js
+++ docs/application.js
@@ -1,2 +1,18 @@
 window.addEventListener('load', () => {
+  const linksToSection = document.querySelectorAll('.js-link-to-section');
+
+  linksToSection.forEach(elm => {
+    elm.addEventListener('click', e => {
+      const target = e.currentTarget;
+      const targetToScroll = document.getElementById(target.getAttribute('href').slice(1));
+
+      e.preventDefault();
+      anime({
+        targets: 'html',
+        scrollTop: targetToScroll.getBoundingClientRect().top + window.pageYOffset,
+        easing: 'linear',
+        duration: 500
+      });
+    });
+  });
 });
