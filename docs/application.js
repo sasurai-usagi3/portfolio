458036349107094d92f827d72607ad73b2a5ff0d
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-05-26 08:54:46 +0900

    update: 画像のアコーディオンやめる

--- docs/application.js
+++ docs/application.js
@@ -16,27 +16,4 @@ window.addEventListener('load', () => {
       });
     });
   });
-
-  btnsToOpenAndCloseList.forEach(elm=> {
-    elm.addEventListener('click', e => {
-      const target = e.currentTarget;
-      const targetToOpenOrClose = document.getElementById(target.dataset.target);
-      const status = target.dataset.status;
-
-      if (status === 'close') {
-        height = '625vh';
-        target.dataset.status = 'open';
-      } else if (status === 'open') {
-        height = '250vh';
-        target.dataset.status = 'close';
-      }
-
-      anime({
-        targets: targetToOpenOrClose,
-        height: height,
-        easing: 'linear',
-        duration: 2000
-      });
-    });
-  });
 });
